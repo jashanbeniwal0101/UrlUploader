f5a81a6ca4e5d59f5f905ceb7999c2b86ceef98f
jashanbeniwal0101 <beniwaljass733@gmail.com>
2025-12-16 07:35:32 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Bot Configuration
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
+API_ID = os.getenv("API_ID", "25331263")
+API_HASH = os.getenv("API_HASH", "cab85305bf85125a2ac053210bcd1030")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGODB_URI = os.getenv("MONGODB_URI", "")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://jeetendra2949_db_user:4FE5y1WlDhZSGV1Y@cluster0.glaqca4.mongodb.net/?appName=Cluster0")
 DB_NAME = os.getenv("DB_NAME", "ytdl_bot_db")
-ADMINS = list(map(int, os.getenv("ADMINS", "").split()))
+ADMINS = list(map(int, os.getenv("ADMINS", "1955406483").split()))
 
 # List of additional IDs to add
 ADDITIONAL_IDS = [123456]  # Add as many as needed
@@ -27,9 +27,9 @@ PAID_USERS = list(map(int, os.getenv("PAID_USERS", "7361945688").split()))
 DEFAULT_UPLOAD_MODE = "video"  # 'video' or 'file'
 DEFAULT_SPLIT_SETTING = True   # Split files larger than 1.95GB
 DEFAULT_THUMBNAIL_GENERATION = True  # Generate thumbnail from video
-DEFAULT_CAPTION_ENABLED = False  # Custom caption
-DEFAULT_GENERATE_SCREENSHOTS = False  # Generate screenshots after upload
-DEFAULT_SAMPLE_VIDEO = False  # Generate sample video
+DEFAULT_CAPTION_ENABLED = True # Custom caption
+DEFAULT_GENERATE_SCREENSHOTS = True # Generate screenshots after upload
+DEFAULT_SAMPLE_VIDEO = True  # Generate sample video
 
 # YT-DLP Default Settings
 DEFAULT_FORMAT = "bestvideo+bestaudio"  # Default format
